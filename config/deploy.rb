027c2f59e4613590bb8aa6c3dd5a6c39df9f311e
DIVE INTO CODE <diveintocode@DIVEnoMacBook-Air.local>
2021-08-18 21:50:44 +0900

    deploy.rb編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :repo_url, 'https://github.com/Aki130238/blogapp'
 set :branch, ENV['BRANCH'] || 'master'
 
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/achieve'
+set :deploy_to, '/var/www/blogapp'
 
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
