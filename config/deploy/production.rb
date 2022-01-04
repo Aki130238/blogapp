120912a6e1574a72127367bbf31b697ee68054fa
DIVE INTO CODE <diveintocode@DIVEnoMacBook-Air.local>
2022-01-04 23:12:28 +0900

    .ssh/id_rsa.pem

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
 server '3.113.206.184', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '/Users/diveintocode/.ssh/id_rsa.pub'
+set :ssh_options, keys: '/Users/diveintocode/.ssh/id_rsa.pem'
