40996305bc00451ef6536330f82ef216bdc3bf84
DIVE INTO CODE <diveintocode@DIVEnoMacBook-Air.local>
2022-01-07 00:03:04 +0900

    cap install

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,2 @@
 server '35.75.218.242', user: 'app', roles: %w{app db web}
-set :ssh_options, { 
-  keys: '/Users/diveintocode/.ssh/id_rsa.pub', 
-  config: false
-}
+set :ssh_options, keys: '/Users/diveintocode/.ssh/id_rsa.pub'
